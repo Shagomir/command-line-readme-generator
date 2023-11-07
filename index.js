f722a9576d8716514bd0b5b0b3329a89085ca622
Shagomir <m.j.knudson@gmail.com>
2023-11-07 13:51:53 -0600

    update with code to generate readme file

--- index.js
+++ index.js
@@ -1,13 +1,99 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// simple regex for email validation from: https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your full name?",
+  },
+  {
+    type: "input",
+    name: "user",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+    validate(value) {
+      const pass = value.match(/^\S+@\S+\.\S+$/);
+      if (pass) {
+        return true;
+      }
+
+      return "Please enter a valid email address.";
+    },
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a description of your project:",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "Please enter the installation instructions for your project:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter usage information for your project:",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Please enter the contribution guidelines for your project:",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please enter test instructions for your project:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select the license you would like to use:",
+    choices: ["MIT", "GNU", "no license"],
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// we create the file in an output folder to avoid overwriting our own README for this application.
+function writeToFile(fileName, data) {
+  //check if our output folder exists. If it doesn't, make it. From https://stackoverflow.com/questions/21194934/how-to-create-a-directory-if-it-doesnt-exist-using-node-js
+  const dir = "./output";
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  //write the file to the output folder.
+  fs.writeFile(`./output/${fileName}`, data, (err) =>
+    err
+      ? console.log(err)
+      : console.log(
+          "Your file has been created. Please check the Outputs folder."
+        )
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    let userAnswers = answers;
+    let readmeText = generateMarkdown(userAnswers);
+    writeToFile("README.md", readmeText);
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,104 @@
+// Source for badges: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+const license = {
+  MIT: {
+    badge:
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    link: "[MIT License](https://opensource.org/licenses/MIT)",
+    text: `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
+  },
+  GNU: {
+    badge:
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    link: "[GNU License](https://www.gnu.org/licenses/gpl-3.0)",
+    text: `This program is free software: you can redistribute it and/or modify 
+    it under the terms of the GNU General Public License as published by 
+    the Free Software Foundation, either version 3 of the License, or 
+    (at your option) any later version. 
+  
+    This program is distributed in the hope that it will be useful, 
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+    GNU General Public License for more details. 
+
+    You should have received a copy of the GNU General Public License 
+    "along with this program.  If not, see <https://www.gnu.org/licenses/>.`,
+  },
+};
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(input) {
+if (input === "MIT") {  return license.MIT.badge }
+else if (input === "GNU") { return license.GNU.badge }
+else {return ""}
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(input) {
+  if (input === "MIT") {  return license.MIT.link }
+else if (input === "GNU") { return license.GNU.link }
+else {return ""}
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(input) {
+  if (input === "MIT") {  return license.MIT.text }
+  else if (input === "GNU") { return license.GNU.text }
+  else {return ""}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+ 
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [License](#license)
+  
+  ## Installation
+  
+  ${data.install}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Contributing
+  
+  ${data.contribution}
+  
+  ## Tests
+  
+  ${data.test}
+  
+  ## Questions
+  
+  Please reach out to me at <${data.email}> with any questions you have about this application. My GitHub profile is located at: <https://github.com/${data.user}>
+  
+  ## License
+
+  ${renderLicenseLink(data.license)}
+  
+  Copyright ${new Date().getFullYear()} ${data.name}
+
+  ${renderLicenseSection(data.license)}
+
 
 `;
 }
